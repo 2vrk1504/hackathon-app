5177fb171643dfd762587ecabe097486b8db1802
omshri22121999 <omshriprasath@gmail.com>
2019-12-21 00:05:21 +0530

    card view

---
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/adapters/CardStackAdapter.java
@@ -0,0 +1,55 @@
+package com.inter_iit_hackathon.hackathon_app.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.inter_iit_hackathon.hackathon_app.R;
+
+import java.util.ArrayList;
+
+public class CardStackAdapter extends RecyclerView.Adapter<CardStackAdapter.ViewHolder> {
+    private ArrayList<String> image_list;
+    private Context context;
+
+
+    public CardStackAdapter(ArrayList<String> i,Context c){
+        this.image_list = i;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View item= layoutInflater.inflate(R.layout.street_card, parent, false);
+        ViewHolder viewHolder = new ViewHolder(item);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(holder.imageView).load(image_list.get(position)).placeholder(R.drawable.ic_launcher_foreground).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.imageView = (ImageView) itemView.findViewById(R.id.image_view);
+        }
+    }
+
+}
