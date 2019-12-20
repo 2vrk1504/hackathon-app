3491272080af039aa17836eedec7dadaab5ed552
2vrk1504 <vallabh.developer@gmail.com>
2019-12-20 10:42:35 +0530

    second commit

--- app/src/main/java/com/inter_iit_hackathon/hackathon_app/MainActivity.java
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
+
     }
 }
