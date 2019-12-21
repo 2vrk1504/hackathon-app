5e6d118ec1dae167656fabc43a12b11120c5cfb6
omshri22121999 <omshriprasath@gmail.com>
2019-12-21 11:00:56 +0530

    Changed login signup and feed

---
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/classes/DashboardData.java
@@ -0,0 +1,37 @@
+package com.inter_iit_hackathon.hackathon_app.classes;
+
+public class DashboardData {
+    String image;
+    String road_name;
+    String description;
+
+    public String getImage() {
+        return image;
+    }
+
+    public DashboardData(String image, String road_name, String description) {
+        this.image = image;
+        this.road_name = road_name;
+        this.description = description;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getRoad_name() {
+        return road_name;
+    }
+
+    public void setRoad_name(String road_name) {
+        this.road_name = road_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
