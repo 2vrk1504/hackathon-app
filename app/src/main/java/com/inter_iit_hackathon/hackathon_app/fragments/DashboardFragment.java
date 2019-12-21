0edfc42adc18e9697f87e7b8641d2c41d353d5bc
omshri22121999 <omshriprasath@gmail.com>
2019-12-21 18:56:14 +0530

    Changed login signup and feed

--- app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/DashboardFragment.java
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/DashboardFragment.java
@@ -4,20 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.inter_iit_hackathon.hackathon_app.GetProjectsQuery;
 import com.inter_iit_hackathon.hackathon_app.R;
 import com.inter_iit_hackathon.hackathon_app.adapters.CardStackAdapter;
 import com.inter_iit_hackathon.hackathon_app.classes.DashboardData;
+import com.inter_iit_hackathon.hackathon_app.graphql_client.MyClient;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
 import com.yuyakaido.android.cardstackview.SwipeAnimationSetting;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -47,14 +56,28 @@ public class DashboardFragment extends Fragment {
         fab_thumbs_up = root.findViewById(R.id.fab_up);
         cardStackLayoutManager = new CardStackLayoutManager(getContext());
         cardStackView.setLayoutManager(cardStackLayoutManager);
-        list.add(new DashboardData("https://source.unsplash.com/user/erondu/900x1600", "Sardar  Patel Road", "Very bad condition, pls halp", "22/12/1999", "LuckyMark123"));
-        list.add(new DashboardData("https://source.unsplash.com/user/erondu/900x1600", "Sardar  Patel Road", "Very bad condition, pls halp", "22/12/1999", "LuckyMark123"));
-        list.add(new DashboardData("https://source.unsplash.com/user/erondu/900x1600", "Sardar  Patel Road", "Very bad condition, pls halp", "22/12/1999", "LuckyMark123"));
-        list.add(new DashboardData("https://source.unsplash.com/user/erondu/900x1600", "Sardar  Patel Road", "Very bad condition, pls halp", "22/12/1999","LuckyMark123"));
-        list.add(new DashboardData("https://source.unsplash.com/user/erondu/900x1600", "Sardar  Patel Road", "Very bad condition, pls halp", "22/12/1999", "LuckyMark123"));
         final CardStackAdapter c = new CardStackAdapter(list, getContext());
         cardStackView.setAdapter(c);
-        c.notifyDataSetChanged();
+        MyClient.getClient().query(GetProjectsQuery.builder().build()).enqueue(new ApolloCall.Callback<GetProjectsQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<GetProjectsQuery.Data> response) {
+                if(response.data()!=null){
+                    List<GetProjectsQuery.Project> project = response.data().projects();
+                    for(int i=0;i<project.size();i++){
+                        list.add(new DashboardData(project.get(i).photo(), project.get(i).title(), project.get(i).description(), project.get(i).updatedAt(), project.get(i).author().name()));
+                    }
+                }
+                else{
+                    Toast.makeText(getContext(),"Sorry, data not available",Toast.LENGTH_SHORT).show();
+                }
+                c.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+            }
+        });
+
         fab_thumbs_down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/FeedFragment.java
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/FeedFragment.java
@@ -1,22 +1,37 @@
 package com.inter_iit_hackathon.hackathon_app.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Query;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.google.android.material.snackbar.Snackbar;
+import com.inter_iit_hackathon.hackathon_app.GetProjectsQuery;
 import com.inter_iit_hackathon.hackathon_app.R;
+import com.inter_iit_hackathon.hackathon_app.SignUpMutation;
+import com.inter_iit_hackathon.hackathon_app.activities.MainActivity;
+import com.inter_iit_hackathon.hackathon_app.activities.SignUpActivity;
 import com.inter_iit_hackathon.hackathon_app.adapters.FeedAdapter;
 import com.inter_iit_hackathon.hackathon_app.classes.FeedClass;
 import com.inter_iit_hackathon.hackathon_app.graphql_client.MyClient;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -40,7 +55,7 @@ public class FeedFragment extends Fragment {
         rview = root.findViewById(R.id.recycler);
         rview.setLayoutManager(new LinearLayoutManager(getContext()));
         ArrayList<FeedClass> f = new ArrayList<FeedClass>();
-//        MyClient.getClient().query().build();
+
         FeedAdapter adapter = new FeedAdapter(f);
         rview.setAdapter(adapter);
         adapter.notifyDataSetChanged();
