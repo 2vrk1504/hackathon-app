0921bab07fddfb2bf0d5fba149e8d6d233af441e
2vrk1504 <vallabh.developer@gmail.com>
2019-12-22 09:54:06 +0530

    added go to location

--- app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/MyMapFragment.java
+++ app/src/main/java/com/inter_iit_hackathon/hackathon_app/fragments/MyMapFragment.java
@@ -1,7 +1,15 @@
 package com.inter_iit_hackathon.hackathon_app.fragments;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -37,6 +45,7 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     private ClusterManager<ClusterPointer> mClusterManager;
     SessionManager sessionManager;
+    LocationManager locationManager;
 
     private List<ClusterPointer> clusterPointers;
     public MyMapFragment() {}
@@ -49,6 +58,7 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sessionManager = new SessionManager(getActivity());
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
     }
 
     @Override
@@ -70,42 +80,62 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.clear();
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(51.5145160,-0.1270060)));
-        mClusterManager = new ClusterManager<>(Objects.requireNonNull(this.getContext()),mMap);
-        mMap.setOnCameraIdleListener(() -> {
-                mClusterManager.cluster();
-        });
-        mMap.setOnMarkerClickListener(mClusterManager);
-        clusterPointers = new ArrayList<>();
-        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<ClusterPointer>() {
-            @Override
-            public boolean onClusterItemClick(ClusterPointer clusterPointer) {
-                Toast.makeText(getContext(), clusterPointer.getPosition().toString(),Toast.LENGTH_SHORT).show();
+        mapSetup();
+
+    }
+
+    void mapSetup(){
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1001);
+        } else {
+            mMap.setMyLocationEnabled(true);
+            mMap.clear();
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            mClusterManager = new ClusterManager<>(Objects.requireNonNull(this.getContext()), mMap);
+            mMap.setOnCameraIdleListener(() -> mClusterManager.cluster());
+            mMap.setOnMarkerClickListener(mClusterManager);
+            clusterPointers = new ArrayList<>();
+            mClusterManager.setOnClusterItemClickListener(clusterPointer -> {
+                Toast.makeText(getContext(), clusterPointer.getPosition().toString(), Toast.LENGTH_SHORT).show();
                 return true;
+            });
+
+            MyClient.getClient(sessionManager.getToken()).query(
+                    GetPostsQuery.builder().build()
+            ).enqueue(new ApolloCall.Callback<GetPostsQuery.Data>() {
+                @Override
+                public void onResponse(@NotNull Response<GetPostsQuery.Data> response) {
+                    List<GetPostsQuery.GetPost> projects = response.data().getPosts();
+                    for (int i = 0; i < projects.size(); i++)
+                        mClusterManager.addItem(new ClusterPointer(projects.get(i).author().name(), projects.get(i).title(), new LatLng(
+                                Double.parseDouble(projects.get(i).location().lat()),
+                                Double.parseDouble(projects.get(i).location().lng())
+                        )));
+
+                    getActivity().runOnUiThread(() -> mClusterManager.cluster());
+
+                }
+
+                @Override
+                public void onFailure(@NotNull ApolloException e) {
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1001: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    mapSetup();
+                }
+                return;
             }
-        });
-
-        MyClient.getClient(sessionManager.getToken()).query(
-                GetPostsQuery.builder().build()
-        ).enqueue(new ApolloCall.Callback<GetPostsQuery.Data>() {
-            @Override
-            public void onResponse(@NotNull Response<GetPostsQuery.Data> response) {
-                List<GetPostsQuery.GetPost> projects = response.data().getPosts();
-                for(int i = 0; i < projects.size(); i++)
-                    mClusterManager.addItem(new ClusterPointer(projects.get(i).author().name(), projects.get(i).title(), new LatLng(
-                            Double.parseDouble(projects.get(i).location().lat()),
-                            Double.parseDouble(projects.get(i).location().lng())
-                    )));
-
-                getActivity().runOnUiThread(() -> mClusterManager.cluster());
-
-            }
-
-            @Override
-            public void onFailure(@NotNull ApolloException e) {
-
-            }
-        });
+        }
     }
 }
